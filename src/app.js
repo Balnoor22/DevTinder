83e81de486b8289f9fa5a8529887511fda56d80b
Balnoor22 <noorroby22@gmail.com>
2025-09-15 21:04:24 +0530

    degraded express v

--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
 const express = require("express");
 
-const app = express(); //creating new instance of express js application using express fxn.This created us a server
+const app = express();
 
-// app.get("/user") -> **This will *only* handle GET call to /user
-app.get("/user", (req, res) => {
+//Now b is optional here,even if we type /ac in url,it will still work  (/abc,/ac)
+app.get("/ab?c", (req, res) => {
   res.send({ firstName: "Balnoor", lastName: "Singh" });
 });
 
-app.post("/user", (req, res) => {
-  //Saving data to DB
-  res.send("Data successfully saved to the database");
+//It means u can add as many b's as u want to (like /abc, /abbbbc, /abbbbbbbbbbbbbbbbbbbc)
+app.get("/ab+c", (req, res) => {
+  res.send({ firstName: "Balnoor", lastName: "Singh" });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
-});
-
-//This app.use() will match all the HTTP method API calls(like GET,POST etc) to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the Server");
-});
-
-//now we want our server to listen through some port no for incoming reqs
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000....");
 });
