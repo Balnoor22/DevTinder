ba1713130832581f04da8a17dadcf41867d43be9
Balnoor22 <noorroby22@gmail.com>
2025-09-15 20:37:52 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ const express = require("express");
 
 const app = express(); //creating new instance of express js application using express fxn.This created us a server
 
-//Now our server will only respond once u go to this url(localhost:3000/test)
+// app.get("/user") -> **This will *only* handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Balnoor", lastName: "Singh" });
+});
+
+app.post("/user", (req, res) => {
+  //Saving data to DB
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
+});
+
+//This app.use() will match all the HTTP method API calls(like GET,POST etc) to /test
 app.use("/test", (req, res) => {
-  //This cb arrow fxn is called Request Handler
   res.send("Hello from the Server");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from the Dashboard!");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello....");
 });
 
 //now we want our server to listen through some port no for incoming reqs
